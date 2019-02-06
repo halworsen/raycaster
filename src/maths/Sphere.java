942db79629275d373139338a93e04da83074263d
Markus <markus@halvorsenfamilien.com>
2019-02-06 18:17:34 +0100

    Compute sphere intersections in a much prettier way

--- src/maths/Sphere.java
+++ src/maths/Sphere.java
@@ -21,48 +21,23 @@ public class Sphere extends GeoShape {
 		return this.radius;
 	}
 	
-	public static double[] solveQuadratic(double a, double b, double c) {
-		double x0, x1;
-		
-		double discr = b*b - 4*a*c;
-		
-		if(discr < 0) {
-			return null;
-		}else if(discr == 0) {
-			x0 = (double)(-0.5 * b/a);
-			x1 = (double)(-0.5 * b/a);
-		}else {
-			double q = (double) ((b > 0) ? (-0.5 * (b + Math.sqrt(discr))) : (-0.5 * (b - Math.sqrt(discr))));
-			x0 = q/a;
-			x1 = c/q;
-		}
-		
-		if(x0 > x1) {
-			double temp = x0;
-			x0 = x1;
-			x1 = temp;
-		}
-		
-		return (new double[] {x0, x1});
-	}
-	
 	public Vector3D getIntersection(Ray ray) {
-		double a = ray.getDirection().dot(ray.getDirection());
-		double b = (2 * (ray.getDirection().dot(ray.getOrigin()) - ray.getDirection().dot(this.origin)));
-		double c = (ray.getOrigin().dot(ray.getOrigin()) + this.origin.dot(this.origin) - 2*ray.getOrigin().dot(this.origin) - Math.pow(this.radius, 2));
+		double projectionLength = ray.getDirection().dot(this.origin.sub(ray.getOrigin()));
+		Vector3D closestPoint = ray.getParametricPoint(projectionLength);
 		
-		double solution[] = solveQuadratic(a,b,c);
-		if(solution == null) {
+		double distToRay = closestPoint.sub(origin).length();
+		// no intersection
+		if(distToRay > radius) {
 			return null;
 		}
 		
-		if(solution[0] < 0) {
-			solution[0] = solution[1];
-			if(solution[0] < 0) {
-				return null;
-			}
+		// ray is tangent to the sphere
+		if(distToRay == radius) {
+			return closestPoint;
 		}
 		
-		return ray.getParametricPoint(solution[0]);
+		double toIntersect = Math.sqrt(Math.pow(radius, 2) - Math.pow(distToRay, 2));
+		
+		return ray.getParametricPoint(projectionLength - toIntersect);
  	}
 }
